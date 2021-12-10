b499d94b4b88fb67e29266b178a2a433dfeb0f73
KaShTaN27 <dkachan.lida@mail.ru>
2021-12-10 22:32:35 +0300

    Task 25 added

---
+++ src/Chapter5/Task25.java
@@ -0,0 +1,22 @@
+package Chapter5;
+
+public class Task25 {
+    public static void main(String[] args) {
+        System.out.println(getLargestPrime(21));
+        System.out.println(getLargestPrime(217));
+        System.out.println(getLargestPrime(0));
+        System.out.println(getLargestPrime(45));
+        System.out.println(getLargestPrime(-1));
+    }
+    public static int getLargestPrime(int number) {
+        if (number < 2)
+            return -1;
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                number /= i;
+                i--;
+            }
+        }
+        return number;
+    }
+}
