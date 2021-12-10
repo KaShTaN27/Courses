8c437206e001f79e5e634acf826fb9d94b4b03d7
KaShTaN27 <dkachan.lida@mail.ru>
2021-12-10 21:06:19 +0300

    Task 22 added

---
+++ src/Chapter5/Task22.java
@@ -0,0 +1,18 @@
+package Chapter5;
+
+public class Task22 {
+    public static void main(String[] args) {
+        System.out.println(isPerfectNumber(6));
+        System.out.println(isPerfectNumber(28));
+        System.out.println(isPerfectNumber(5));
+        System.out.println(isPerfectNumber(-1));
+    }
+    public static boolean isPerfectNumber(int number) {
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0)
+                sum += i;
+        }
+        return number > 1 && number == sum;
+    }
+}
